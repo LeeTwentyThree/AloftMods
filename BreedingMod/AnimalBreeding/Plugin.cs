4dafa9bf3ebd0ae3699f5a2b38e0b7c7d0facbbc
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-02-04 21:50:55 -0500

    Add harmony patching to breeding mod

--- BreedingMod/AnimalBreeding/Plugin.cs
+++ BreedingMod/AnimalBreeding/Plugin.cs
@@ -1,5 +1,7 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
 
 namespace AnimalBreeding;
 
@@ -7,11 +9,16 @@ namespace AnimalBreeding;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
         
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(Assembly);
     }
 }
